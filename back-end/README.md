8ca538483b0af0c9dc7fcbec1b38319e0941db5c
jpfalcuci <jpfalcuci@gmail.com>
2023-09-18 20:22:04 -0300

    feat: Criação dos CRUDs básicos restantes

--- back-end/README.md
+++ back-end/README.md
@@ -35,3 +35,21 @@
 ## Exibir os dados cadastrados com o Prisma Studio
 `npx prisma studio`
 - Deve ser executado em um segundo terminal, enquanto o projeto está sendo executado no primeiro
+
+## Criar um novo CRUD a partir de outro já existente
+
+### _Controller_
+
+1. Copie o arquivo do _controller_ (`Ctrl+C Ctrl+V`) e renomeie-o de acordo com a nova entidade para a qual será feito o novo CRUD.
+2. Substitua (`Ctrl+H`) todas as ocorrências de `prisma.xxxx` por `prisma.yyyy`, onde `xxxx` é o nome da entidade antiga e `yyyy` é o nome da entidade nova.
+3. Verifique, no método `revieveAll()` da entidade, se há campos sendo usados no _orderby_ e substitua-os conforme a nova entidade.
+
+### _Route_
+
+1. Copie o arquivo da _route_ (`Ctrl+C Ctrl+V`) e renomeie-o de acordo com a nova entidade para a qual será feito o novo CRUD.
+2. Na linha de `import` do _controller_, substitua o nome da entidade antiga pelo nome dda entidade nova.
+
+### _Arquivo 'app.js'_
+
+1. Na parte inferior do arquivo, na seção de rotas, copie e cole as duas linhas correspondentes a uma rota já existentes e faça as substituições necessárias.
+    * São **quatro substituições** ao todo, duas em cada linha.
---
+++ back-end/src/controllers/aluno.js
@@ -0,0 +1,87 @@
+import prisma from '../database/client.js';
+
+const controller = {};  // Objeto vazio
+
+controller.create = async function (req, res) {
+  try {
+    // Conecta-se ao BD e envia uma instrução de criação de um novo documento, com os dados que estão dentro de req.body
+    await prisma.aluno.create({data: req.body})
+    // Envia uma resposta de sucesso ao front-end
+    res.status(201).end();  // HTTP 201: Created
+  } catch (error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error);
+    // Envia uma resposta de erro ao front-end
+    res.status(500).send(error);  // HTTP 500: Internal Server Error
+  }
+}
+
+controller.retrieveAll = async function (req, res) {
+  try {
+    // Manda buscar os dados no servidor
+    // Retorna o resultado ordenado por nome, depois nível
+    const result = await prisma.aluno.findMany({
+      orderBy: [
+        {nome: 'asc'}
+      ]
+    });
+    res.send(result); // HTTP 200: Ok (implícito)
+  } catch (error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error);
+    // Envia uma resposta de erro ao front-end
+    res.status(500).send(error);  // HTTP 500: Internal Server Error
+  }
+}
+
+controller.retrieveOne = async function (req, res) {
+  try {
+    // Manda buscar os dados no servidor
+    const result = await prisma.aluno.findUnique({
+      where: { id: req.params.id }
+    });
+
+    if (result) res.send(result);   // Encontrou ~> Retorna HTTP 200: Ok
+    else res.status(404).end();     // Não encontrou ~> Retorna HTTP 404: Not Found
+  } catch (error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error);
+    // Envia uma resposta de erro ao front-end
+    res.status(500).send(error);  // HTTP 500: Internal Server Error
+  }
+}
+
+controller.update = async function (req, res) {
+  try {
+    const result = await prisma.aluno.update({
+      where: { id: req.params.id },
+      data: req.body
+    });
+
+    if (result) res.status(204).end();  // HTTP 204: No Content
+    else res.status(404).end();         // Não encontrou (e não atualizou) ~> Retorna HTTP 404: Not Found
+  } catch (error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error);
+    // Envia uma resposta de erro ao front-end
+    res.status(500).send(error);  // HTTP 500: Internal Server Error
+  }
+}
+
+controller.delete = async function (req, res) {
+  try {
+    const result = await prisma.aluno.delete({
+      where: { id: req.params.id }
+    });
+
+    if (result) res.status(204).end();  // HTTP 204: No Content
+    else res.status(404).end();         // Não encontrou (e não excluiu) ~> Retorna HTTP 404: Not Found
+  } catch (error) {
+    // Deu errado: exibe o erro no console do back-end
+    console.error(error);
+    // Envia uma resposta de erro ao front-end
+    res.status(500).send(error);  // HTTP 500: Internal Server Error
+  }
+}
+
+export default controller;
